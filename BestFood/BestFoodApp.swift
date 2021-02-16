ba8bd6d3df707ddbbaec46398f9462572f2220a2
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-02-16 10:14:16 +0100

    Initial Commit

---
+++ BestFood/BestFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestFoodApp.swift
+//  BestFood
+//
+//  Created by Łukasz Andrzejewski on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct BestFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestFood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BestFood
+//
+//  Created by Łukasz Andrzejewski on 16/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
